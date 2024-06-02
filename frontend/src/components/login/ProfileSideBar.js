7ce7f02c535cbbd835d1995fa8da4a414454b1ba
kira0826 <curaca492@gmail.com>
2024-06-01 21:09:51 -0500

    Avoid use of <a> for navigating

--- frontend/src/components/login/ProfileSideBar.js
+++ frontend/src/components/login/ProfileSideBar.js
@@ -1,74 +1,70 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { useUser } from "../../UserContext.js";
 
-function ProfileSideBar() {
+function ProfileSideBar({ onComponentChange }) {
+  const user = useUser();
 
-    const user = useUser();
-    
+  return (
+    <div className="col-span-3">
+      <div className="px-4 py-3 shadow flex items-center gap-4">
+        <div className="flex-shrink-0">
+          <img
+            src={user ? user.image : "../assets/images/avatar.png"}
+            alt="profile"
+            className="rounded-full w-14 h-14 border border-gray-200 p-1 object-cover"
+          />
+        </div>
+        <div className="flex-grow">
+          <p className="text-gray-600">Hello,</p>
+          <h4 className="text-gray-800 font-medium">
+            {user ? user.firstName : "not a name"}
+          </h4>
+        </div>
+      </div>
 
-    return (
-        <div className="col-span-3">
-        <div className="px-4 py-3 shadow flex items-center gap-4">
-            <div className="flex-shrink-0">
-                <img src={user?user.image :"../assets/images/avatar.png"} alt="profile"
-                    className="rounded-full w-14 h-14 border border-gray-200 p-1 object-cover"/>
-            </div>
-            <div className="flex-grow">
-                <p className="text-gray-600">Hello,</p>
-                <h4 className="text-gray-800 font-medium">{user? user.firstName : "not a name" }</h4>
-            </div>
+      <div className="mt-6 bg-white shadow rounded p-4 divide-y divide-gray-200 space-y-4 text-gray-600">
+        <div className="space-y-1 pl-8">
+          <a
+            onClick={() => onComponentChange("ProfileData")}
+            className="relative hover:text-primary block capitalize transition font-medium"
+          >
+            <span className="absolute -left-8 top-0 text-base">
+              <i className="fa-regular fa-address-card"></i>
+            </span>
+            Manage account
+          </a>
+
+          <a className="relative hover:text-primary block capitalize transition">
+            Change password
+          </a>
         </div>
 
-        <div className="mt-6 bg-white shadow rounded p-4 divide-y divide-gray-200 space-y-4 text-gray-600">
-            <div className="space-y-1 pl-8">
-                <a href="/compunet1FinalProject/profile" className="relative hover:text-primary block capitalize transition font-medium">
-                    <span className="absolute -left-8 top-0 text-base">
-                        <i className="fa-regular fa-address-card"></i>
-                    </span>
-                    Manage account
-                </a>
-        
-                <a href="#" className="relative hover:text-primary block capitalize transition">
-                    Change password
-                </a>
-            </div>
-
-            <div className="space-y-1 pl-8 pt-4">
-                <a href="/compunet1FinalProject/orderHistory" className="relative hover:text-primary block font-medium capitalize transition">
-                    <span className="absolute -left-8 top-0 text-base">
-                        <i className="fa-solid fa-box-archive"></i>
-                    </span>
-                    My order history
-                </a>
-              
-            </div>
-
-            <div className="space-y-1 pl-8 pt-4">
-                <a className="relative hover:text-primary block font-medium capitalize transition">
-                    <span className="absolute -left-8 top-0 text-base">
-                        <i className="fa-regular fa-credit-card"></i>
-                    </span>
-                    Payment methods
-                </a>
-                <a href="#" className="relative hover:text-primary block capitalize transition">
-                    voucher
-                </a>
-            </div>
-
-
-            <div className="space-y-1 pl-8 pt-4">
-                <a href="#" className="relative hover:text-primary block font-medium capitalize transition">
-                    <span className="absolute -left-8 top-0 text-base">
-                        <i className="fa-solid fa-right-from-bracket"></i>
-                    </span>
-                    Logout
-                </a>
-            </div>
-
+        <div className="space-y-1 pl-8 pt-4">
+          <a
+            onClick={() => onComponentChange("OrderHistory")}
+            className="relative hover:text-primary block font-medium capitalize transition"
+          >
+            <span className="absolute -left-8 top-0 text-base">
+              <i className="fa-solid fa-box-archive"></i>
+            </span>
+            My order history
+          </a>
         </div>
+
+        <div className="space-y-1 pl-8 pt-4">
+          <a className="relative hover:text-primary block font-medium capitalize transition">
+            <span className="absolute -left-8 top-0 text-base">
+              <i className="fa-regular fa-credit-card"></i>
+            </span>
+            Payment methods
+          </a>
+          <a className="relative hover:text-primary block capitalize transition">
+            voucher
+          </a>
+        </div>
+      </div>
     </div>
-    );
-
+  );
 }
 
-export default ProfileSideBar    
+export default ProfileSideBar;
--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Layout } from "./index.js";
-import { ProfileData, ProfileSideBar } from "../components/index.js";
+import { ProfileData, ProfileSideBar, OrderHistory } from "../components/index.js";
+
 
 function Profile() {
+  const [selectedComponent, setSelectedComponent] = useState("ProfileData");
+
+  const handleComponentChange = (component) => {
+    setSelectedComponent(component);
+  };
+  
+
   return (
-    
-      <div className="container grid grid-cols-12 items-start gap-6 mt-4 pt-4 pb-16">
-        <ProfileSideBar />
-        <ProfileData />
-      </div>
+    <div className="container grid grid-cols-12 items-start gap-6 mt-4 pt-4 pb-16">
+      <ProfileSideBar onComponentChange={handleComponentChange} />
+      {selectedComponent === "ProfileData" && <ProfileData />}
+      {selectedComponent === "OrderHistory" && <OrderHistory />}
+    </div>
   );
 }
 
