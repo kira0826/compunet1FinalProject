7e50db021463877ef51aae02c78593624bae84ee
santiesleo <83185169+santiesleo@users.noreply.github.com>
2024-06-01 23:15:46 -0500

    VerificationPopup and receipts

--- backend/index.js
+++ backend/index.js
@@ -1,43 +1,36 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import PropertiesReader from "properties-reader";
+import { products } from "./com/products.js";
+import writeSomethingToFile from "./com/Writer.js";
 import multer from "multer";
-import { MongoClient,ServerApiVersion } from "mongodb";
-import dotenv from "dotenv";
 
-dotenv.config();
+import { users } from "./users/users.js";
+import { receipts } from "./receipts/receipts.js";
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+// fileURLToPath is used because we are using module ES.
 
-const port = process.env.PORT || 18900
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const propertiesPath = path.resolve(__dirname, "../config.properties");
+import upload from "./Storage/Storage.js";
+const userFileInfo = {
+  path: `./users/users.js`,
+  variableName: `users`,
+}
+const productsFileInfo = {
+  path: `./com/products.js`,
+  variableName: `products`,
+}
 
-const uri = process.env.DATABASE_URL
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+const properties = PropertiesReader(propertiesPath);
+const port = properties.get("app.port");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-const connectDB = async () => {
-  try {
-    await client.connect();
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("Error connecting to MongoDB", error);
-  }
-};
-
-connectDB();
-
-const productsCollection = client.db("FinalProjectDatabase").collection("products");
-const usersCollection = client.db("FinalProjectDatabase").collection("users");
-const ordersCollection = client.db("FinalProjectDatabase").collection("orders");
 
 app.listen(port, () => {
   console.log("Server listen in " + port);
@@ -45,42 +38,30 @@ app.listen(port, () => {
 
 //GET
 
-app.get("/products", async (req, res) => {
-  const products = await productsCollection.find().toArray();
+app.get("/products", (req, res) => {
   res.status(200).json(products);
 });
 
-app.get("/products/:id", async (req, res) => {
-  const { id } = req.params;
-  console.log("GET /products/:id", id);
-
-  const product =  await productsCollection.findOne({ id: Number(id) });
-  res.status(200).json({ product, message: "product successfully got." });
+app.get("/receipts", (req, res) => {
+  res.status(200).json(receipts);
 });
 
-app.get("/orderHistory/:idUser", async (req, res) => {
-  const { idUser } = req.params;
-  console.log("GET /orderHistory/:idUser", idUser);
-
-  const order = await ordersCollection.findOne({idUser : idUser});
-  console.log("order", order);
-  res.status(200).json({ order, message: "order successfully got." });
+app.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+  console.log("GET /products/:id", id);
+  const product = products.find((elem) => elem.id === Number(id));
+  res.status(200).json({ product, message: "product successfully got." });
 });
 
 //POST
 
-app.post("/products", upload.single("image"), async (req, res) => {
-  const file = req.file;
-  console.log("POST /products", req.body);
+app.post("/products", upload.single("image"), (req, res) => {
   const { body } = req;
-  
-  const count = await productsCollection.countDocuments();
-  const newProductId = count + 1;
-
+  //const image = './Storage/imgs' + req.file.originalname
   const newProduct = {
-    id: newProductId,
+    id: products.length + 1,
     stock: body.stock,
-    image: body.image,
+    image: "./Storage/imgs/" + body.image,
     discount: body.discount,
     category: body.category,
     brand: body.brand,
@@ -88,58 +69,26 @@ app.post("/products", upload.single("image"), async (req, res) => {
     price: body.price,
     description: body.description,
   };
-  console.log("newProduct", newProduct);
-  if (newProduct.name != null) {
-    await productsCollection.insertOne(newProduct);
+
+  if (newProduct.id != null) {
+    products.push(newProduct);
   }
 
   res.send({});
 });
 
-app.post("/order", async (req, res) => {
-  console.log("POST /order ESTOOOOY");
-  const file = req.file;
-  console.log("POST /orders", req.body);
-  const { body } = req;
-
-  const userOrders = await ordersCollection.findOne({idUser : body.idUser});
-  
-  const newIdOrder = userOrders? userOrders.orders.length : 0;
-
-  const newOrder ={
-    idOrder: newIdOrder,
-    date: body.orders.date , // Fecha actual en formato YYYY-MM-DD
-    products: body.orders.products
-  };
-
-  if (newOrder.products != null) {
-    if (userOrders) {
-      await ordersCollection.findOneAndUpdate(
-        { idUser: body.idUser },
-        { $push: { orders: newOrder } }
-      );
-    } else {
-      await ordersCollection.insertOne({
-        idUser: body.idUser,
-        orders: [newOrder]
-      });
-    }
-    
-    res.status(201).json({ message: "order successfully created." });
-  }
-});
-
-app.post("/login", async (req, res) => {
+app.post("/login", (req, res) => {
   console.log("POST /login");
 
   const { password, email } = req.body;
 
-  const user = await usersCollection.findOne({ email });
-
+  const user = users.find(
+    (elem) => elem.email === String(email) && elem.password === String(password)
+  );
 
   console.log(user);
 
-  if (user && user['password']=== password) {
+  if (user) {
     console.log("melo", user);
 
     res.status(200).json({ user, message: "Login successful" }); // Send user object and "OK" message
@@ -150,7 +99,30 @@ app.post("/login", async (req, res) => {
   }
 });
 
-app.post("/register", async (req, res) => {
+app.post("/receipt", (req, res) => {
+  const { user, products} = req.body;
+
+  products.forEach(element => {
+    console.log('product: ' + element)
+  });
+
+  if (!products) {
+    return res
+      .status(400)
+      .json({ message: "Todos los campos son obligatorios" });
+  }
+
+  const newReceipt = {
+    user: user,
+    products: req.body.products,
+  };
+
+  receipts.push(newReceipt);
+
+  res.status(201).json({ message: "Factura registrada exitosamente" });
+});
+
+app.post("/register", (req, res) => {
   const { name, email, password, confirmPass } = req.body;
   const [firstName, lastName] = String(name).split(" ");
 
@@ -164,9 +136,10 @@ app.post("/register", async (req, res) => {
     return res.status(400).json({ message: "Las contraseñas no coinciden" });
   }
 
-  const existingUser = await usersCollection.findOne({ email });
+  const existingUser = users.find((elem) => elem.email === email);
+
   if (existingUser) {
-    return res.status(400).json({ message: "User already exists" });
+    return res.status(400).json({ message: "El usuario ya existe" });
   }
 
   const newUser = {
@@ -176,53 +149,83 @@ app.post("/register", async (req, res) => {
     password: password,
   };
 
-  await usersCollection.insertOne(newUser);
+  users.push(newUser);
+  writeSomethingToFile( userFileInfo.path, userFileInfo.variableName, users);
 
   console.log("Usuario registrado:", newUser);
 
   res.status(201).json({ message: "Usuario registrado exitosamente" });
 });
+
 //PATCH
 
-app.patch("/products/:id", upload.single("image"), async (req, res) => {
+app.patch("/products/:id", (req, res) => {
   console.log("PATCH /EditProduct");
 
   const { id } = req.params;
   const body = req.body;
   console.log("body", body);
 
-  try {
-    const result = await productsCollection.updateOne(
-      { id: Number(id) },
-      { $set: body }
-    );
-    if (result.matchedCount > 0) {
-      const product = await productsCollection.findOne({ id: Number(id) });
-      res.status(200).json({ product, message: "Product updated successfully" });
-    } else {
-      res.status(404).json({ message: "Product not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: "Failed to update product" });
+  const productIndex = products.findIndex((elem) => elem.id === Number(id));
+
+  if (productIndex === -1) {
+    res.status(404).json({ message: "Product not found" });
+    return;
   }
+
+  const product = products[productIndex];
+
+
+  for (const key in body) {
+    product[key] = body[key];
+  }
+
+  products[productIndex] = product;
+  writeSomethingToFile( productsFileInfo.path, productsFileInfo.variableName, products);
+
+  res.status(200).json({ message: "Login successful" });
 });
 
+
+
 //PUT
+app.put("/products/:id", (req, res) => {
+  const { id } = req.params;
+
+  console.log("PUT /products/:id", id)
+
+  const updatedProduct = req.body;
+
+  console.log("producto upd: " + updatedProduct.stock)
+
+  const productIndex = products.findIndex((elem) => elem.id === Number(id));
+
+  if (productIndex !== -1) {
+    products[productIndex] = { ...products[productIndex], ...updatedProduct };
+    res.status(200).json({ product: products[productIndex], message: "Product successfully updated." });
+  } else {
+    res.status(404).json({ message: "Product not found." });
+  }
+})
+
 
 //DELETE
 
-app.delete("/products/:id", async (req, res) => {
+app.delete("/products/:id", (req, res) => {
   const { id } = req.params;
   console.log("DELETE /products/:id", id);
 
-  try {
-    const result = await productsCollection.deleteOne({ id: Number(id) });
-    if (result.deletedCount > 0) {
-      res.status(200).json({ message: "Product deleted successfully" });
-    } else {
-      res.status(404).json({ message: "Product not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: "Failed to delete product" });
+  const productIndex = products.findIndex((elem) => elem.id === Number(id));
+
+  if (productIndex === -1) {
+    res.status(404).json({ message: "Product not found" });
+    return;
   }
+
+  products.splice(productIndex, 1);
+  writeSomethingToFile( productsFileInfo.path, productsFileInfo.variableName, products);
+
+  console.log("Despues", products);
+
+  res.status(200).json({ message: "Product successfully deleted" });
 });
--- frontend/src/components/checkout/Receipt.js
+++ frontend/src/components/checkout/Receipt.js
@@ -1,213 +1,173 @@
-import React, { useState } from "react";
-import { useCheckout, useCheckoutUpdate } from "../../CheckoutContext.js";
-import { useUser } from "../../UserContext.js";
-import Popup from "../general/Popup.js";
+import React, {useState} from "react";  
+import { useCheckout, useCheckoutUpdate} from '../../CheckoutContext.js';
+import config from "../../config.json";
+import VerificationPopup from "../general/VerificationPopup.js"
 
 // substractCar -> func que viene de checkout
 // para restarle cantidad al carrito cada vez
 // que quite un producto
 function Receipt({ substractCart }) {
-  const [showPopup, setShowPopup] = useState(false);
+    const [showPopup, setShowPopup] = useState(false);
+    const updateCheckout = useCheckoutUpdate(); // funcion updateCheckout
 
-  const handleCancelDelete = () => {
-    console.log("Cancelando popup");
-    setShowPopup(false);
-  };
+    const storedUser = localStorage.getItem("user");
+    console.log("user: " + storedUser);
 
-  const handleConfirmDelete = () => {
-    console.log("confirmar popup");
-    setShowPopup(false);
-  };
-  const apiUrl =
-    process.env.NODE_ENV === "production"
-      ? process.env.REACT_APP_URL_PROD
-      : process.env.REACT_APP_URL_LOCAL;
-  const user = useUser();
-  const updateCheckout = useCheckoutUpdate(); // funcion updateCheckout
+    // costo de envio
+    var shipping = 0;
 
-  // costo de envio
-  var shipping = 0;
+    // obtenemos productos del checkout context
+    const checkout = useCheckout();
 
-  // obtenemos productos del checkout context
-  const checkout = useCheckout();
+    // calculamos subtotal
+    const subtotal = () => {
+        var sum = 0;
 
-  // calculamos subtotal
-  const subtotal = () => {
-    var sum = 0;
+        checkout.map((product) => {
+            sum += product.price
+        })
 
-    checkout.map((product) => {
-      sum += parseInt(product.price);
-    });
+        return sum;
+    }
 
-    return sum;
-  };
+    // calculamos total
+    const total = () => {
+        return shipping + subtotal();
+    }
 
-  // calculamos total
-  const total = () => {
-    return shipping + subtotal();
-  };
+    // Estilos para los botones
+    const buttonStyles = {
+        backgroundColor: '#fd3d57',
+        color: 'white',
+        fontWeight: 'bold',
+        padding: '0.2rem 0.7rem', // Hacer el botón más pequeño
+        borderRadius: '0.25rem',
+        fontSize: '0.75rem' // Tamaño de la fuente más pequeño
+    };
 
-  // Estilos para los botones
-  const buttonStyles = {
-    backgroundColor: "#fd3d57",
-    color: "white",
-    fontWeight: "bold",
-    padding: "0.2rem 0.7rem", // Hacer el botón más pequeño
-    borderRadius: "0.25rem",
-    fontSize: "0.75rem", // Tamaño de la fuente más pequeño
-  };
+    const onAccept = () => {
+        setShowPopup(false);
+    }
+    
+    const handleDeleteClick = (id) => {
+        updateCheckout(id, 0);
+        substractCart();
+    }
 
-  const handleDeleteClick = (id) => {
-    updateCheckout(id, 0);
-    substractCart();
-  };
+    const handlePlaceOrderClick = async () => {
+        var products = [];
 
-  const handlePlaceOrderClick = async () => {
-    if (user) {
-      let tempProducts = checkout;
-      console.log("Productos en el carrito:", tempProducts);
-      if (tempProducts.length === 0) {
-        setShowPopup(true);
-      } else {
-        const orderData = {
-          idUser: user.email,
-          orders: {
-            date: new Date().toISOString().split("T")[0],
-            products: tempProducts.map((product) => ({
-              idProduct: product.id,
-              quantity: 1, // Ajustar la cantidad según sea necesario
-            })),
-          },
+        for (const product of checkout) {
+            products.push(product);
+            const updatedProduct = { ...product, stock: product.stock - 1 };
+
+            try {
+                const response = await fetch(config["app.api"] + "/products/" + product.id, {
+                    method: "PUT",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(updatedProduct),
+                });
+
+                if (!response.ok) {
+                    throw new Error('Error al actualizar el producto');
+                }
+
+                // Si la actualización es exitosa, actualiza localmente el checkout
+                substractCart();
+                updateCheckout(product.id, updatedProduct.stock);
+            } catch (error) {
+                console.error('Error al actualizar el producto:', error);
+            }
+        }
+
+        products.forEach(element => {
+            console.log('product: ' + element.id)
+        });
+
+        const receipt = {
+            user: storedUser,
+            products: products
         };
 
-        console.log("Datos de la orden:", orderData);
-        if (tempProducts.length > 0) {
-          try {
-            const response = await fetch(apiUrl + "/order", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(orderData),
+        console.log('receipt: ' + receipt.products)
+
+        try {
+            const response = await fetch(config["app.api"] + "/receipt/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(receipt),
             });
 
             if (!response.ok) {
-              throw new Error("Error al crear la orden");
+                throw new Error('Error al crear factura');
             }
-
-            // Si la creación es exitosa, vaciar el carrito
-            //checkout.forEach((product) => substractCart());
-            console.log("Orden creada exitosamente");
-          } catch (error) {
-            console.error("Error al crear la orden:", error);
-          }
+            
+            setShowPopup(true);
+        } catch (error) {
+            console.error('Error al crear factura:', error);
         }
-        for (const product of checkout) {
-          const updatedProduct = { ...product, stock: product.stock - 1 };
-          const changedValues = {};
-          changedValues["stock"] = updatedProduct.stock;
-          const formData = new FormData();
-          formData.append("stock", updatedProduct.stock);
-          try {
-            const response = await fetch(apiUrl + "/products/" + product.id, {
-              method: "PATCH",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(changedValues),
-            });
+    };
 
-            if (!response.ok) {
-              throw new Error("Error al actualizar el producto");
-            }
-
-            // Si la actualización es exitosa, actualiza localmente el checkout
-            substractCart();
-            updateCheckout(product.id, updatedProduct.stock);
-          } catch (error) {
-            console.error("Error al actualizar el producto:", error);
-          }
-        }
-      }
-    } else {
-      setShowPopup(true);
-    }
-  };
-
-  return (
-    <div className="col-span-4 border border-gray-200 p-4 rounded">
-      <h4 className="text-gray-800 text-lg mb-4 font-medium uppercase">
-        order summary
-      </h4>
-      <div className="space-y-2">
-        {checkout.map((product) => (
-          <div className="flex justify-between items-center" key={product.id}>
-            <div>
-              <h5 className="text-gray-800 font-medium">{product.name}</h5>
-              <p className="text-sm text-gray-600">Size: {product.size}</p>
+    return (
+        <div className="col-span-4 border border-gray-200 p-4 rounded">
+            <h4 className="text-gray-800 text-lg mb-4 font-medium uppercase">order summary</h4>
+            <div className="space-y-2">
+                {checkout.map((product) => (
+                    <div className="flex justify-between items-center" key={product.id}>
+                        <div>
+                            <h5 className="text-gray-800 font-medium">{product.name}</h5>
+                            <p className="text-sm text-gray-600">Size: {product.size}</p>
+                        </div>
+                        <div className="flex items-center">
+                            <button style={buttonStyles} className="mr-2 hover:bg-transparent hover:text-primary"
+                            onClick={() => handleDeleteClick(product.id)}>
+                                -
+                            </button>
+                            <p className="text-gray-800 font-medium">${product.price}</p>
+                        </div>
+                    </div>
+                ))}
             </div>
-            <div className="flex items-center">
-              <button
-                style={buttonStyles}
-                className="mr-2 hover:bg-transparent hover:text-primary"
-                onClick={() => handleDeleteClick(product.id)}
-              >
-                -
-              </button>
-              <p className="text-gray-800 font-medium">${product.price}</p>
+
+            <div className="flex justify-between border-b border-gray-200 mt-1 text-gray-800 font-medium py-3 uppercas">
+                <p>subtotal</p>
+                <p>${subtotal()}</p>
             </div>
-          </div>
-        ))}
-      </div>
 
-      <div className="flex justify-between border-b border-gray-200 mt-1 text-gray-800 font-medium py-3 uppercas">
-        <p>subtotal</p>
-        <p>${subtotal()}</p>
-      </div>
+            <div className="flex justify-between border-b border-gray-200 mt-1 text-gray-800 font-medium py-3 uppercas">
+                <p>shipping</p>
+                <p>Free</p>
+            </div>
 
-      <div className="flex justify-between border-b border-gray-200 mt-1 text-gray-800 font-medium py-3 uppercas">
-        <p>shipping</p>
-        <p>Free</p>
-      </div>
+            <div className="flex justify-between text-gray-800 font-medium py-3 uppercas">
+                <p className="font-semibold">Total</p>
+                <p>${total()}</p>
+            </div>
 
-      <div className="flex justify-between text-gray-800 font-medium py-3 uppercas">
-        <p className="font-semibold">Total</p>
-        <p>${total()}</p>
-      </div>
+            <div className="flex items-center mb-4 mt-2">
+                <input type="checkbox" name="aggrement" id="aggrement"
+                    className="text-primary focus:ring-0 rounded-sm cursor-pointer w-3 h-3"/>
+                <label htmlFor="aggrement" className="text-gray-600 ml-3 cursor-pointer text-sm">I agree to the <a href="#"
+                        className="text-primary">terms & conditions</a></label>
+            </div>
+            
+            <a onClick={() => handlePlaceOrderClick()}
+                className="block w-full py-3 px-4 text-center text-white bg-primary border border-primary rounded-md hover:bg-transparent hover:text-primary transition font-medium">Place
+                order</a>
 
-      <div className="flex items-center mb-4 mt-2">
-        <input
-          type="checkbox"
-          name="aggrement"
-          id="aggrement"
-          className="text-primary focus:ring-0 rounded-sm cursor-pointer w-3 h-3"
-        />
-        <label
-          htmlFor="aggrement"
-          className="text-gray-600 ml-3 cursor-pointer text-sm"
-        >
-          I agree to the{" "}
-          <a href="#" className="text-primary">
-            terms & conditions
-          </a>
-        </label>
-      </div>
+            {showPopup && (
+                <VerificationPopup
+                message="Gracias por tu compra. Los productos llegarán pronto a su destino."
+                onAccept={onAccept} products={checkout}
+                />
+            )}
+        </div>
+    )
 
-      <a
-        onClick={() => handlePlaceOrderClick()}
-        className="block w-full py-3 px-4 text-center text-white bg-primary border border-primary rounded-md hover:bg-transparent hover:text-primary transition font-medium"
-      >
-        Place order
-      </a>
-      {showPopup && (
-        <Popup
-          message="Por favor, valide que tiene una sesión activa y productos en el carrito"
-          onConfirm={handleConfirmDelete}
-          onCancel={handleCancelDelete}
-        />
-      )}
-    </div>
-  );
 }
 
 export default Receipt;
